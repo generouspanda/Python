f3985d99bff8b520bfc3a19677a5b82acb729eff
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-10-28 08:58:31 -0400

    first commit

---
+++ Bigger or Smaller.py
@@ -0,0 +1,6 @@
+a=(input("First Number: "))
+b=(input("Second Number: "))
+if(a<b):
+    print("Your first number is smaller than your second")
+else:
+    print("Your second number is smaller than your first")
