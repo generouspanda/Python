70bda7c741e7836c5d4a7589f91f5c5258261a83
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-10-28 22:21:08 -0400

    Added changes

--- operations.py
+++ operations.py
@@ -128,7 +128,7 @@ def equation():
                 print("Your slope is: 0")
                 printed=str(f"y={d-b}")
                 print(printed)
-                graphing() 
+                graphing() 32
 
             if(int(a)==0) and (int(c)==0) or ((float(a)-float(c))==0):
                 slope=0
