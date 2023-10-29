f40c3f6d516cdb4fe38171f3df34ad8f562998fb
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-10-29 09:07:49 -0400

    Added changes

--- BiggerorSmaller.py
+++ BiggerorSmaller.py
@@ -3,4 +3,4 @@ b=(input("Second Number: "))
 if(a<b):
     print("Your first number is smaller than your second")
 else:
-    print("Your second number is smaller than your first") hi
+    print("Your second number is smaller than your first") 
