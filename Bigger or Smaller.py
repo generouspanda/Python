cfa928c521438c51e34369004b87b194be78e314
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-10-29 09:19:28 -0400

    Delete Bigger or Smaller.py

--- Bigger or Smaller.py
+++
@@ -1,6 +0,0 @@
-a=(input("First Number: "))
-b=(input("Second Number: "))
-if(a<b):
-    print("Your first number is smaller than your second")
-else:
-    print("Your second number is smaller than your first")
