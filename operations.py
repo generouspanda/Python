603350671bf2288ec0752091fce0fbd9eaadd22f
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-11-05 10:22:28 -0600

    Added changes

--- operations.py
+++ operations.py
@@ -107,10 +107,7 @@ def equation():
                 print("Please answer with Y or N.")
                 continue
     while True:
-        a = convert(a_1)
-        b = convert(b_1)
-        c = convert(c_1)
-        d = convert(d_1)
+        
         yn=input(f"Are your coodinates ({a}, {b}) ({c}, {d}): ")
 
         try:
