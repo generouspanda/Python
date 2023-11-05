ed1a926e07a42ee50281fef6d18e0176f0ce2686
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-11-05 10:20:22 -0600

    Added changes

--- operations.py
+++ operations.py
@@ -154,7 +154,7 @@ def equation():
                     print("Please answer with Y or N.")
                     continue
         except ValueError as e:
-            print("")
+            print("Something went wrong")
             equation()
 def whichone():
     options = ["Operations", "Solve for the equation of a line"]
