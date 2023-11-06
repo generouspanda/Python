2f1270a0eb5cac615b03060e4ff9af0fb3078302
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-11-06 07:41:08 -0600

    Added changes

--- operations.py
+++ operations.py
@@ -142,7 +142,7 @@ def equation():
                     print("Your slope is:", slope_strip)
                     print(f"Your equation in point-slope form is: y-{str(b).rstrip('.0')}={slope_strip}(x-{str(a).rstrip('.0')})")
                     printed=str(f"y={slope_strip}x+{yint_strip}")
-                                       print(f"Your equation in standard form is: {slope_strip}x+y={yint_strip}")
+                    print(f"Your equation in standard form is: {slope_strip}x+y={yint_strip}")
                     print(f"Your equation in slope-intercept form is: {printed}")      
  
                     graphing()
