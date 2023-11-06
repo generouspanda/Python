7d5ab700a179e676f0dad5a7368ea1eb6f4c4613
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-11-06 07:47:31 -0600

    Added changes

--- operations.py
+++ operations.py
@@ -97,7 +97,7 @@ def equation():
                         try:
                             from pyautogui import press, typewrite, hotkey
                             import webbrowser
-        import time
+                            import time
                             webbrowser.open_new("https://www.desmos.com/calculator")
                             time.sleep(1)
                             typewrite(printed)
