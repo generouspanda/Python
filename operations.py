64e222a44eea6fbd28836d665fa4379fc69aad22
generouspanda <106203982+generouspanda@users.noreply.github.com>
2023-11-07 23:12:29 -0500

    Added changes

--- operations.py
+++ operations.py
@@ -10,6 +10,7 @@ def another():
             if(yn=="y") or (yn=="Y") or (yn=="yes") or (yn=="Yes"):
                 whichone()
             if(yn=="n") or (yn=="N") or (yn=="no") or (yn=="No"):
+                import
                 exit()
             else:
                 print("Please answer with Y or N.")
